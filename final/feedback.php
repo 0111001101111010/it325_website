cfdebff439a667fc21412179abea7e8f112277a0
stanzheng <dev.stanzheng@gmail.com>
2014-04-25 03:36:13 -0400

    added feedback test

---
+++ final/feedback.php
@@ -0,0 +1,20 @@
+<?php
+//echo "foo doo<br>";
+date_default_timezone_set('EST5EDT');
+$my_file = 'feedback.txt';
+$handle = fopen($my_file, 'a') or die('Cannot open file:'.$my_file);
+$data = file_get_contents($my_file);
+//print($data);
+//var_dump($_POST);
+if(isset($_POST["name"])){
+//add the most material
+$file_data = "Time: ". date("m/d/Y h:i:s a", time()) ."\nName: ".$_POST["name"] . "\n" . "Comment: ".$_POST["comment"] . "\nEmail:". " " . $_POST["email"]. "\n\n";
+$file_data .= file_get_contents($my_file);
+file_put_contents($my_file, $file_data);
+// $data = '\n New data line 1';
+// fwrite($handle, $data);
+// $new_data = "\n".'New data line 2';
+// fwrite($handle, $new_data);
+ }
+ header('Location:a9.php?submitted');
+?>
